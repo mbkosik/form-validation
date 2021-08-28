dd7cd320f80a26c0862e866cb37a8d820fd83e7b
mbkosik <72941144+mbkosik@users.noreply.github.com>
2021-08-28 22:15:48 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,85 @@
+const send = document.getElementById("send");
+const clear = document.getElementById("clear");
+const close = document.querySelector(".close");
+
+const inputs = [...document.querySelectorAll(".form-data")];
+const password = document.getElementById("password");
+const passwordRepeat = document.getElementById("password-repeat");
+const email = document.getElementById("email");
+
+const registerBox = document.querySelector(".register");
+const popup = document.querySelector(".popup");
+const registerForm = document.querySelector(".register__form");
+
+const errorText = document.createElement("p");
+let condition = false; 
+
+const checkForm = (e) => {
+    e.preventDefault(); 
+    resetError(); 
+
+    for (i = 0; i < inputs.length; i++) {
+        if (inputs[i].value === "") {
+            return displayError("Uzupełnij puste pola");
+        } 
+    } 
+
+    for (i = 0; i < inputs.length; i++) {
+        let min = 5;
+        if (inputs[i].value.length < min) {
+            let msg = `Dane powinny mieć ponad ${min} znaków`;
+            return displayError(msg);
+        }
+    } 
+
+    if (password.value !== passwordRepeat.value) {
+        return displayError("Popraw hasło");
+    }
+
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const regexTest = regex.test(email.value);
+
+    if (regexTest === false) {
+        return displayError("E-mail jest niepoprawny");
+    }
+
+    submitForm();
+}
+
+function clearForm() {
+    inputs.forEach((input) => {
+        input.value = "";
+    })
+    resetError();
+}
+
+function submitForm() {
+    clearForm();
+    popup.classList.add("show-popup");
+}
+
+function closePopUp() {
+    popup.classList.remove("show-popup");
+}
+
+function displayError(msg) {
+    errorText.classList.add("error-text");
+    registerForm.appendChild(errorText);
+    errorText.textContent = msg;
+    registerBox.classList.add("error");
+    setTimeout(function() { 
+        registerBox.classList.remove("error"); 
+    }, 2000)
+    condition = true; 
+}
+
+function resetError() {
+    if (condition === true) {
+        document.querySelector("p.error-text").remove(); 
+        condition = false; 
+    }
+}
+
+send.addEventListener("click", checkForm); 
+clear.addEventListener("click", clearForm);
+close.addEventListener("click", closePopUp);
